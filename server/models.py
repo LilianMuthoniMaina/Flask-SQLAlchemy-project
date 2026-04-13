4870706e508ea1b02b8c2debce2ecc79234dfef0
MFSA-KE-CMITU <mfsa-ke-cmitu@MFSA-KE-CMITUs-MacBook-Pro.local>
2026-04-13 22:00:00 +0300

    models

---
+++ server/models.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+db = SQLAlchemy()
+
+# Define Models here
+class Exercise(db.model):
+    __tablename__ = 'exercises '
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment = db.Column(db.Boolean)
+
+
+class Workout(db.model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
+
+
+class WorkoutExercises(db.model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(primary_key=True)
+    workout_id = db.Column(db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.ForeignKey('exercises.id'))
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
